fd31d8bed05928ed0856b8fd980073dda1d84974
Elliot Williams <elliotdwilliams@gmail.com>
2023-08-07 16:37:47 -0500

    Initial commit

---
+++ dpla-subject-search.py
@@ -0,0 +1,25 @@
+from time import sleep
+from dpla.api import DPLA
+from credentials import *
+
+# Create DPLA object using dpla module and your API key
+dpla = DPLA(DPLA_KEY)
+
+# Read subjects file
+file = open('subjects.txt', 'r')
+subjects = file.readlines()
+
+# Iterate through each subject
+with open('results.txt', 'w') as results_file:
+    for subject in subjects:
+        subject = subject.strip()
+        print(subject)
+        results = dpla.search(subject, fields=["sourceResource.subject.name"])
+        count = results.count
+        print(count)
+
+        results_file.write(subject+'\t'+str(count)+'\n')
+
+        sleep(0.5)
+
+results_file.close()
