cf7faaa32e9ab23b429376debde3c168ab5b7e74
Elliot Williams <elliotdwilliams@gmail.com>
2023-08-14 11:46:19 -0500

    Adds facet_size to dpla-subject-search

--- dpla-subject-search.py
+++ dpla-subject-search.py
@@ -23,7 +23,7 @@ with open('results.txt', 'w') as results_file:
         print(subject)
 
         api_url = ('https://api.dp.la/v2/items?sourceResource.subject.name=%22' + subject_url + 
-                   '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&exact_field_match=true')
+                   '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&facet_size=100&exact_field_match=true')
         response = requests.get(api_url).json()
 
         count = response['count']
