e01b2de318a92ac56fe3ddae1d3539cac061e3bc
Elliot Williams <elliotdwilliams@gmail.com>
2023-08-18 12:05:01 -0500

    Add length-checking to subjects before making API calls

--- dpla-subject-search.py
+++ dpla-subject-search.py
@@ -1,5 +1,5 @@
-''' Queries DPLA API to find out how many records use a given subject term, and how many 
-institutions those records are contributed by. Accepts a list of subjects, and outputs a 
+''' Queries DPLA API to find out how many records use a given subject term, and how many
+institutions those records are contributed by. Accepts a list of subjects, and outputs a
 tab-delimited file with subjects, number of records, and number of contributors.
 '''
 
@@ -19,22 +19,26 @@ with open('results.txt', 'w') as results_file:
 # Iterate through each subject
     for subject in subjects:
         subject = subject.strip()
-        subject_url = quote(subject) # URL-encode
-        print(subject)
+        if (len(subject) <= 198):
+            subject_url = quote(subject)  # URL-encode
+            print(subject)
 
-        api_url = ('https://api.dp.la/v2/items?sourceResource.subject.name=%22' + subject_url + 
-                   '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&facet_size=100&exact_field_match=true')
-        response = requests.get(api_url).json()
+            api_url = ('https://api.dp.la/v2/items?sourceResource.subject.name=%22' + subject_url +
+                       '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&facet_size=100&exact_field_match=true')
+            response = requests.get(api_url).json()
 
-        count = response['count']
-        print(count)
+            count = response['count']
+            print(count)
 
-        contributors = response['facets']['dataProvider']['terms']
-        contributor_count = len(contributors)
-        print(contributor_count)
+            contributors = response['facets']['dataProvider']['terms']
+            contributor_count = len(contributors)
+            print(contributor_count)
 
-        results_file.write(subject + '\t' + str(count) + '\t' + str(contributor_count) + '\n')
+            results_file.write(subject + '\t' + str(count) + '\t' + str(contributor_count) + '\n')
 
-        sleep(0.25)
+            sleep(0.25)
+        else:
+            print(subject + '\nERROR - String length not valid')
+            results_file.write(subject + '\tERROR - String length not valid\n')
 
 results_file.close()
--- dpla-uniq-subjects.py
+++ dpla-uniq-subjects.py
@@ -6,10 +6,11 @@ from credentials import *
 # Set parameters for API call
 # In source, for hub, use 'provider='; for institutions, use 'dataProvider='
 source = 'dataProvider="UTSA Libraries Special Collections"'
+facet_size = 1000  # number of subjects to retrieve, max 2000
 
 # Create API call and make request
 api_url = ('https://api.dp.la/v2/items?' + source + '&api_key=' + DPLA_KEY
-           + '&facets=sourceResource.subject.name&facet_size=1000&page_size=0')
+           + '&facets=sourceResource.subject.name&facet_size=' + facet_size + '&page_size=0')
 print(api_url)
 response = requests.get(api_url).json()
 
@@ -25,24 +26,29 @@ with open('uniq_subjects.txt', 'w') as results_file:
 # Iterate through each subject
     for subject in subjects:
         subject_term = subject['term'].strip()
-        subject_url = quote(subject_term) # URL-encode
 
-        # Make second API call to get contributors per subject term
-        api_url = ('https://api.dp.la/v2/items?sourceResource.subject.name=%22' + subject_url +
-                   '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&facet_size=20&exact_field_match=true&page_size=0')
-        response = requests.get(api_url).json()
+        if (len(subject_term) <= 198):
+            subject_url = quote(subject_term)  # URL-encode
 
-        # Count number of contributors for this term
-        contributors = response['facets']['dataProvider']['terms']
-        contributor_count = len(contributors)
-        print(subject_term + ' ' + str(contributor_count))
+            # Make API call to get contributors per subject term
+            api_url = ('https://api.dp.la/v2/items?sourceResource.subject.name=%22' + subject_url +
+                       '%22&api_key=' + DPLA_KEY + '&facets=dataProvider&facet_size=20&exact_field_match=true&page_size=0')
+            response = requests.get(api_url).json()
 
-        # If unique, write to results file
-        if contributor_count == 1:
-            results_file.write(subject_term + '\n')
-            number_uniq += 1 # add 1 to counter
+            # Count number of contributors for this term
+            contributors = response['facets']['dataProvider']['terms']
+            contributor_count = len(contributors)
+            print(subject_term + ' ' + str(contributor_count))
 
-        sleep(0.1)
+            # If unique, write to results file
+            if contributor_count == 1:
+                results_file.write(subject_term + '\n')
+                number_uniq += 1  # add 1 to counter
+
+            sleep(0.1)
+
+        else:
+            print(subject_term + ' ERROR: String too long')
 
     percent_uniq = "{:.2%}".format(number_uniq/number_subjects)
     print('***' + percent_uniq + ' unique***')
